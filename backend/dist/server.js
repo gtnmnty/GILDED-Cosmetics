075d1806a719bddbc98171f49433975fd015fb1d
Monty <bgwhiskey64@gmail.com>
2026-05-05 00:11:46 +0800

    remov type module and fix imports

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -5,17 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const accordion_js_1 = require("./data/about-us/accordion.js");
-const reviews_js_1 = require("./data/about-us/reviews.js");
-const account_payments_js_1 = require("./data/faqs/account-payments.js");
-const appointments_booking_js_1 = require("./data/faqs/appointments-booking.js");
-const orders_delivery_js_1 = require("./data/faqs/orders-delivery.js");
-const products_aftercare_js_1 = require("./data/faqs/products-aftercare.js");
-const services_pricing_js_1 = require("./data/faqs/services-pricing.js");
-const shipping_returns_js_1 = require("./data/faqs/shipping-returns.js");
-const services_js_1 = require("./data/services.js");
-const products_js_1 = require("./data/products.js");
-const deliveryOption_js_1 = require("./data/deliveryOption.js");
+const accordion_1 = require("./data/about-us/accordion");
+const reviews_1 = require("./data/about-us/reviews");
+const account_payments_1 = require("./data/faqs/account-payments");
+const appointments_booking_1 = require("./data/faqs/appointments-booking");
+const orders_delivery_1 = require("./data/faqs/orders-delivery");
+const products_aftercare_1 = require("./data/faqs/products-aftercare");
+const services_pricing_1 = require("./data/faqs/services-pricing");
+const shipping_returns_1 = require("./data/faqs/shipping-returns");
+const services_1 = require("./data/services");
+const products_1 = require("./data/products");
+const deliveryOption_1 = require("./data/deliveryOption");
 const mongoose_1 = __importDefault(require("mongoose"));
 const mongoURI = 'mongodb+srv://Monty:Dbnip777@cluster0.wcj1q1o.mongodb.net/?appName=Cluster0';
 mongoose_1.default.connect(mongoURI)
@@ -31,33 +31,33 @@ app.get('/', (req, res) => {
 });
 // About Us Page
 app.get('/api/about/accordion', (req, res) => {
-    res.json(accordion_js_1.accordions);
+    res.json(accordion_1.accordions);
 });
 // Faqs Page
 app.get('/api/faqs', (req, res) => {
     res.json({
-        accountPayments: account_payments_js_1.accountPayments,
-        appointmentsBooking: appointments_booking_js_1.appointmentsBooking,
-        ordersDelivery: orders_delivery_js_1.ordersDelivery,
-        productsAftercare: products_aftercare_js_1.productsAftercare,
-        servicesPricing: services_pricing_js_1.servicesPricing,
-        shippingReturns: shipping_returns_js_1.shippingReturns,
+        accountPayments: account_payments_1.accountPayments,
+        appointmentsBooking: appointments_booking_1.appointmentsBooking,
+        ordersDelivery: orders_delivery_1.ordersDelivery,
+        productsAftercare: products_aftercare_1.productsAftercare,
+        servicesPricing: services_pricing_1.servicesPricing,
+        shippingReturns: shipping_returns_1.shippingReturns,
     });
 });
 // Services
 app.get('/api/services', (req, res) => {
-    res.json(services_js_1.services);
+    res.json(services_1.services);
 });
 // Products
 app.get('/api/products', (req, res) => {
-    res.json(products_js_1.products);
+    res.json(products_1.products);
 });
 // Delviery Options
 app.get('/api/services', (req, res) => {
-    res.json(deliveryOption_js_1.deliveryOpts);
+    res.json(deliveryOption_1.deliveryOpts);
 });
 app.get('/api/about/reviews', (req, res) => {
-    res.json(reviews_js_1.reviews);
+    res.json(reviews_1.reviews);
 });
 app.listen(PORT, () => {
     console.log(`🚀 Server is flying at http://localhost:${PORT}`);
